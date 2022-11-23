7ce8343c0bfbc35430881624270147b4c5bae8e5
yannrichet <richetyann@gmail.com>
2022-11-23 13:57:39 +0100

    move functions exmaples in subdir

--- docs/source/make_examples.sh
+++ docs/source/make_examples.sh
@@ -11,9 +11,9 @@ for f in `ls functions`; do
     echo $f
     
     echo "library(rlibkriging)" > /tmp/$f.R
-    echo "png('examples/$f.png')"        >> /tmp/$f.R
+    echo "png('functions/examples/$f.png')"        >> /tmp/$f.R
     cat functions/$f | sed '1,/Examples/d' | sed '1,/```r/d'  | sed '/```/,$d' >> /tmp/$f.R
     echo "w<-dev.off()"            >> /tmp/$f.R
 
-    Rscript /tmp/$f.R > examples/$f.Rout
+    Rscript /tmp/$f.R > functions/examples/$f.Rout
 done
