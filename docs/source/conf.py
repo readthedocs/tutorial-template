54212e6730229569e5e5c29041e8455421e884a0
Benjamin Balder Bach <benjaoming@gmail.com>
2023-01-13 16:43:57 +0100

    Remove sphinx.ext.duration, only works since Sphinx 2 (not py2 compatible)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,7 +12,7 @@ version = '0.1.0'
 # -- General configuration
 
 extensions = [
-    'sphinx.ext.duration',
+#    'sphinx.ext.duration',
     'sphinx.ext.doctest',
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
